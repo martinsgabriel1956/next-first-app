f0b8ea3927df048433e91be50886c2015e44b0a0
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-15 08:53:01 -0300

    feat: more on ssg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,34 @@
-import { MeetupList } from '../components/Meetups/MeetupList';
+import { MeetupList } from "../components/Meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
 ];
 
 export function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export async function getStaticProps() {
   // fetch data from an API
   return {
     props: {
-      meetups: DUMMY_MEETUPS
-    }
-  }; 
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
 }
