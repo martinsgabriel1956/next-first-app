cc528323bdbd415852823e721ca35cd46a9f9ea9
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-15 09:00:20 -0300

    feat: exploring server side rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,19 @@ export function HomePage() {
   return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
 export async function getStaticProps() {
   // fetch data from an API
   return {
